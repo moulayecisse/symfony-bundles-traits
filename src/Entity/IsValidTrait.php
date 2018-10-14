9a5d919847906a9409b33dddb1a4a3989057af48
Moulaye Cissé <moulaye.c@gmail.com>
2018-10-14 19:06:29 +0200

    Add Amount, Comment and IsValid

---
+++ src/Entity/IsValidTrait.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moulaye
+ * Date: 27/08/2018
+ * Time: 01:03
+ */
+
+namespace Cisse\Traits\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+Trait IsValidTrait
+{
+    /**
+     * IsValid
+     *
+     * @var bool
+     *
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isValid;
+
+    /**
+     * Set isValid
+     *
+     * @param  bool $isValid
+     *
+     * @return self
+     */
+    public function setIsValid(bool $isValid) : self
+    {
+        $this->isValid = $isValid;
+
+        return $this;
+    }
+
+    /**
+     * Get isValid
+     *
+     * @return bool
+     */
+    public function getIsValid() : bool
+    {
+        return $this->isValid;
+    }
+}
