b6d2cc2648c774c7801dc2fe495530f65213bf25
moulaye <moulaye.c@gmail.com>
2018-10-20 23:40:48 +0200

    Implemented ValueTrait

---
+++ src/Entity/ValueTrait.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moulaye
+ * Date: 27/08/2018
+ * Time: 01:03
+ */
+
+namespace Cisse\Traits\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+Trait ValueTrait
+{
+    /**
+     * Value
+     *
+     * @var string
+     *
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $Value;
+
+    /**
+     * Set Value
+     *
+     * @param  string $Value
+     *
+     * @return self
+     */
+    public function setValue(string $Value) : self
+    {
+        $this->Value = $Value;
+
+        return $this;
+    }
+
+    /**
+     * Get Value
+     *
+     * @return string
+     */
+    public function getValue() : string
+    {
+        return $this->Value;
+    }
+}
