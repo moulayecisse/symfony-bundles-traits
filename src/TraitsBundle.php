b767c52af88e7c6d9a2da6cd805ab73f93a5bb6f
Moulaye <moulaye.c@gmail.com>
2019-02-17 09:15:11 +0100

    [FIX] Fix bundle

---
+++ src/TraitsBundle.php
@@ -0,0 +1,14 @@
+<?php
+namespace Cisse\Traits;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TraitsBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new TraitsPass());
+    }
+}
