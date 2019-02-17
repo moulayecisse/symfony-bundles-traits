ec2a6ce85029748252f8bf30c08b95a27a02ca27
Moulaye <moulaye.c@gmail.com>
2019-02-17 13:14:56 +0100

    [FIX] Fix CompilerPass

--- src/TraitsCompilerPass.php
+++ src/TraitsCompilerPass.php
@@ -13,10 +13,10 @@ class TraitsCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        if ($container->hasParameter('twig.form.resources')) {
-            $resources = $container->getParameter('twig.form.resources') ?: [];
-            array_unshift($resources, '@Recaptcha/fields.html.twig');
-            $container->setParameter('twig.form.resources', $resources);
-        }
+//        if ($container->hasParameter('twig.form.resources')) {
+//            $resources = $container->getParameter('twig.form.resources') ?: [];
+//            array_unshift($resources, '@Recaptcha/fields.html.twig');
+//            $container->setParameter('twig.form.resources', $resources);
+//        }
     }
 }
